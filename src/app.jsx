ca303f7f98a166b6b0ff01991b456f265469b4bd
Tsa2341 <alanshema2002@gmail.com>
2024-04-28 11:48:56 +0200

    added color and font themes

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import { ColorModeContext, useMode } from './themeConfig';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Button, CssBaseline, ThemeProvider, Typography } from '@mui/material';
 import Topbar from './global/Topbar';
 
 function App() {
@@ -9,12 +9,16 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="App">
+        <div className="h-full w-full flex relative">
           {/* the topbar */}
-          <main className="main-content">
+          <main className="main-content w-full">
             <Topbar />
 
             {/* routes will be defined here */}
+            <Typography variant="display" className="text-primary font-semibold">
+              Hello World
+            </Typography>
+            <Button>Hello click me</Button>
           </main>
         </div>
       </ThemeProvider>
