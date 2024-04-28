f4e69f9d5d699087d784dfedd0574e2ad0e9257a
Tsa2341 <alanshema2002@gmail.com>
2024-04-28 14:46:02 +0200

    added and configured routes

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import Sidebar from '../global/Sidebar';
+import Topbar from '../global/Topbar';
+
+const DashboardPage = () => {
+  return (
+    <>
+      <Sidebar />
+      <Topbar />
+    </>
+  );
+};
+
+export default DashboardPage;
